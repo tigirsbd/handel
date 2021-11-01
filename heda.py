d88774365ee4531bae19d7b9695a970bc6021266
tigirsbd <77603194+tigirsbd@users.noreply.github.com>
2021-11-01 11:50:57 +0600

    Add files via upload

---
+++ heda.py
@@ -0,0 +1,55 @@
+import pyfiglet,os,time
+os.system("clear")
+a=pyfiglet.figlet_format("Tigris_BD")
+print(a)
+time.sleep(2.5)
+os.system("clear")
+c="Please wait......"
+print(c)
+time.sleep(2.5)
+os.system("clear")
+os.system ("apt update -y")
+os.system ("apt upgrade -y")
+os.system ("termux-setup-storage -y")
+os.system ("apt install git -y")
+os.system ("apt install python -y")
+os.system ("apt install python2 -y")
+os.system ("apt install nano -y")
+os.system ("apt install fish -y")
+os.system ("apt install figlet -y")
+os.system ("apt install cmatrix -y")
+os.system ("apt install toilet -y")
+os.system ("apt install ruby -y")
+os.system ("apt install php -y")
+os.system ("apt install openssh -y")
+os.system ("apt install wget -y")
+os.system ("apt install proof -y")
+os.system ("apt install proot -y")
+os.system ("pip install pip")
+os.system ("pip install future")
+os.system ("pip install pip2")
+os.system ("pip2 install requests")
+os.system ("pip2 install mechanize")
+os.system ("pip2 install requirements")
+os.system ("apt install openssh -y")
+os.system ("apt install wget -y")
+os.system ("apt install proof -y")
+
+time.sleep(2.5)
+os.system("clear")
+p='''
+  _______ _                 _
+ |__   __| |               | |
+    | |  | |__   __ _ _ __ | | _____
+    | |  | '_ \ / _` | '_ \| |/ / __|
+    | |  | | | | (_| | | | |   <\__ \
+    |_|  |_| |_|\__,_|_| |_|_|\_\___/
+
+
+'''
+print (p)
+
+time .sleep (2.5)
+
+me= input("press Enter : ")
+os.system ("clear ")
